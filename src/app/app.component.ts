29c28044bc7957cc1b74cd7a3bf18da7de1eb96b
VinitaSonwane <VinitaSonwane>
2024-09-05 11:30:21 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+
+  num : number = 10;
+  num1 = 22.22;
+
+  str :string = "Hello";
+  str1 = "Hello";
+
+  bool:boolean = true;
+  bool1 = true;
+  greet = " Hello World"
+
+  //Dclare array
+  arr = ["Java" , "Angular" , "Python" , "Android"];
+
+  student = {
+  name : "John",
+  age : 22,
+  city : "Pune" , 
+  marks: 80
+  }
+
+  hello(){
+    console.log("Hello World")
+    return this.greet
+  }
+
+  employee_info =[
+  {
+empmo:11,
+empName:"Alice",
+empCountry:"India",
+empSalary:4500
+  },
+  {
+    empmo:12,
+    empName:"Bob",
+    empCountry:"US",
+    empSalary:5500
+      },
+      {
+        empmo:13,
+        empName:"Ram",
+        empCountry:"UK",
+        empSalary:6500
+          },
+          {
+            empmo:14,
+            empName:"Sham",
+            empCountry:"India",
+            empSalary:7500
+              },
+
+
+]
+
 }
